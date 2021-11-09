310bd519cc69308ff4eeb0a4c4d9aa5baf0d3bee
yamini530 <92918147+yamini530@users.noreply.github.com>
2021-11-09 14:32:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("This is Java program in git hub executed from jenkins job!");
   }
 }
